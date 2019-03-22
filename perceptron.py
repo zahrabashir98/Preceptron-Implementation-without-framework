f43534f3bcf39c21d72994b2b175431d1a5502ca
zahrabashir98 <zahra_9877@yahoo.com>
2019-03-22 18:50:08 +0430

    Add a different parsing way

--- perceptron.py
+++ perceptron.py
@@ -2,13 +2,15 @@ import numpy as np
 
 class Perceptron(object):
 
-    def __init__(self, no_of_inputs, threshold=100, learning_rate=0.01):
+    def __init__(self, no_of_inputs, threshold=5000, learning_rate=0.001):
         self.threshold = threshold
         self.learning_rate = learning_rate
         self.weights = np.zeros(no_of_inputs + 1)
            
     def predict(self, inputs):
         summation = np.dot(inputs, self.weights[1:]) + self.weights[0]
+        # print(self.weights[0])
+        # print("here")
         if summation > 0:
           activation = 1
         else:
@@ -20,4 +22,4 @@ class Perceptron(object):
             for inputs, label in zip(training_inputs, labels):
                 prediction = self.predict(inputs)
                 self.weights[1:] += self.learning_rate * (label - prediction) * inputs
-self.weights[0] += self.learning_rate * (label - prediction)
+                self.weights[0] += self.learning_rate * (label - prediction)
